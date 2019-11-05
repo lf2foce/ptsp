3848cf68489d103010f7fcaccc19f95cfe843e43
Anh Bui <lf2foce@gmail.com>
2019-11-05 18:09:29 +0700

    du chuc nang user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,13 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
   def create
     @user = current_user
     @post = @user.posts.create(post_params)
+    redirect_to @user
+  end
+  def destroy
+    @user = current_user
+    @post = @user.posts.find(params[:id])
+    @post.destroy
     redirect_to @user
   end
  
