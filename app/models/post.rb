a2c5483eb5a6d40e45e41459568df2cf2fa2b23e
Anh Bui <lf2foce@gmail.com>
2019-11-06 22:44:02 +0700

    styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   validates :title, presence: true,
-                    length: { minimum: 5 }
+                    length: { minimum: 3 }
 
 	after_create_commit :post_pending                
 	scope :approved, -> { where(post_status: "approved") }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-   has_many :posts, dependent: :destroy
+  has_many :posts, dependent: :destroy
 
-   	#scope :approved, -> { where(post_status: "approved") }
 	scope :pending, -> { 
 		joins(:posts).where(posts: {post_status: "pending"})
 	}
-	#scope :rejected, -> { where(post_status: "rejected") }
+	scope :rejected, -> { 
+		joins(:posts).where(posts: {post_status: "rejected"})
+	}
+	scope :approved, -> { 
+		joins(:posts).where(posts: {post_status: "approved"})
+	}
+
 	scope :manager, -> { where(role: "manager") }
 	scope :sale, -> { where(role: "sale") }
 	
